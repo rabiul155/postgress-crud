8fe325126a6bf46781842f2bed4204650e74bfbe
rabiul155 <siyamict@gmail.com>
2025-02-12 22:32:30 +0600

    fix issues

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const userRoutes = require('./routes/user')
+const userRoutes = require('./routes/user');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 
 //middleware 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 app.get('/', (req, res) => {
   res.send('PostgreSQL CRUD API is running...');
